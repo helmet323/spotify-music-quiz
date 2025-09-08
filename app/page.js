ba80023124dba27caab8a4660482ba8782178d67
Tommy Su <81478928+helmet323@users.noreply.github.com>
2025-09-07 21:46:46 -0400

    initial commit

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">Spotify Music Quiz</h1>
+    </div>
+  );
+}
